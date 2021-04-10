e07901866e2d4e87b64210d02087b65b4d4b937c
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-04-10 16:31:15 -0700

    pc/bt -- address eslint issues

--- frontend/src/stories/pages/HomePage/HomePage.stories.js
+++ frontend/src/stories/pages/HomePage/HomePage.stories.js
@@ -3,8 +3,6 @@ import React from 'react';
 
 import HomePage from "main/pages/HomePage/HomePage";
 
-import { QueryClient, QueryClientProvider } from "react-query";
-import { MemoryRouter } from "react-router-dom";
 import ContextTemplate from "stories/storyutils/ContextTemplate"
 
 export default {
@@ -12,20 +10,8 @@ export default {
     component: HomePage
 };
 
-
 const HomePageInContext = ContextTemplate(HomePage);
 const Template = () => <HomePageInContext />;
-
-// const Template = (args) =>  {
-//     const queryClient = new QueryClient();
-//     return (
-//         <QueryClientProvider client={queryClient}>
-//             <MemoryRouter>
-//                 <HomePage {...args} />
-//             </MemoryRouter>
-//         </QueryClientProvider>
-//     )
-// };
 
 export const Default = Template.bind({});
 
