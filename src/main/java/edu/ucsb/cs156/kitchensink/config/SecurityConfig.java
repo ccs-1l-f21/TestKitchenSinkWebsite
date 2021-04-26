738d87fe05940e1333cb9636fcd033f613c98187
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-04-25 20:43:52 -0700

    pc - WIP on admin emails

--- src/main/java/edu/ucsb/cs156/kitchensink/config/SecurityConfig.java
+++ src/main/java/edu/ucsb/cs156/kitchensink/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package edu.ucsb.cs156.kitchensink.config;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,6 +26,8 @@ import org.springframework.security.web.authentication.Http403ForbiddenEntryPoin
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import edu.ucsb.cs156.kitchensink.entities.User;
+import edu.ucsb.cs156.kitchensink.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
@@ -28,25 +35,26 @@ import lombok.extern.slf4j.Slf4j;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Value("${app.admin.emails}")
+  final private List<String> adminEmails = new ArrayList<String>();
+
+  @Autowired
+  UserRepository userRepository;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests(authorize -> authorize
-          .mvcMatchers("/api/docs").permitAll()
-          .mvcMatchers("/api/admin/**").hasRole("ADMIN")
-          .mvcMatchers("/api/**").permitAll()
-          .anyRequest().permitAll()
-        )
-        .exceptionHandling(handlingConfigurer -> handlingConfigurer
-          .authenticationEntryPoint(new Http403ForbiddenEntryPoint())
-        )
-        .oauth2Login( oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo.userAuthoritiesMapper(this.userAuthoritiesMapper())))
-        .csrf(csrf -> csrf
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-        )
-        .logout(logout -> logout
-            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-            .logoutSuccessUrl("/")
-        );
+          // .mvcMatchers("/api/docs").permitAll()
+          // .mvcMatchers("/api/admin/**").hasRole("ADMIN")
+          // .mvcMatchers("/api/**").permitAll()
+          .anyRequest().permitAll())
+        .exceptionHandling(
+            handlingConfigurer -> handlingConfigurer.authenticationEntryPoint(new Http403ForbiddenEntryPoint()))
+        .oauth2Login(
+            oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo.userAuthoritiesMapper(this.userAuthoritiesMapper())))
+        .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+        .logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/"));
     ;
   }
 
@@ -55,41 +63,50 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     web.ignoring().antMatchers("/h2-console/**");
   }
 
+  public boolean isAdmin(String email) {
+    if (adminEmails.contains(email)) {
+      return true;
+    }
+    Optional<User> u = userRepository.findByEmail(email);
+    return u.isPresent() && u.get().isAdmin();
+  }
+
   private GrantedAuthoritiesMapper userAuthoritiesMapper() {
     return (authorities) -> {
-        Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+      Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
 
-        authorities.forEach(authority -> {
-            log.info("********** authority={}",authority);
-            if (OidcUserAuthority.class.isInstance(authority)) {
-                OidcUserAuthority oidcUserAuthority = (OidcUserAuthority)authority;
+      authorities.forEach(authority -> {
+        log.info("********** authority={}", authority);
 
-                OidcIdToken idToken = oidcUserAuthority.getIdToken();
-                OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
+        if (OidcUserAuthority.class.isInstance(authority)) {
+          OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) authority;
 
-                log.info("********** idToken={}",idToken);
-                log.info("********** userInfo={}",userInfo);
+          OidcIdToken idToken = oidcUserAuthority.getIdToken();
+          OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
 
-                // Map the claims found in idToken and/or userInfo
-                // to one or more GrantedAuthority's and add it to mappedAuthorities
+          log.info("********** idToken={}", idToken);
+          log.info("********** userInfo={}", userInfo);
 
-            } else if (OAuth2UserAuthority.class.isInstance(authority)) {
-                OAuth2UserAuthority oauth2UserAuthority = (OAuth2UserAuthority)authority;
+          // Map the claims found in idToken and/or userInfo
+          // to one or more GrantedAuthority's and add it to mappedAuthorities
 
-                Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
-                log.info("********** userAttributes={}",userAttributes);
+        } else if (OAuth2UserAuthority.class.isInstance(authority)) {
+          OAuth2UserAuthority oauth2UserAuthority = (OAuth2UserAuthority) authority;
 
-                if (userAttributes.get("email").equals("phtcon@ucsb.edu")) {
-                  mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-                }
-                // Map the attributes found in userAttributes
-                // to one or more GrantedAuthority's and add it to mappedAuthorities
-            }
-        });
+          Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
+          log.info("********** userAttributes={}", userAttributes);
 
-        return mappedAuthorities;
+          String email = (String) userAttributes.get("email");
+          if (isAdmin(email)) {
+            mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+          }
+
+          // Map the attributes found in userAttributes
+          // to one or more GrantedAuthority's and add it to mappedAuthorities
+        }
+
+      });
+      return mappedAuthorities;
     };
-}
-
-
+  }
 }
